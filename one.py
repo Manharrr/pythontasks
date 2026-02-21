21e4336e4ae336cec08b88613ee2a91150d8c190
ManharCK <manhargurukkal2@gmail.com>
2026-02-21 15:36:24 +0530

    first

---
+++ one.py
@@ -0,0 +1,59 @@
+# stud={"name":"manhar","batch":57}
+
+# stud["pincode"]=67601
+# stud.update({"pin":676301})
+
+# print(stud)
+
+
+
+# print(aa.slice([2:]))
+
+# s={1,2,3} b={3,4,5}
+
+# print(set(a.union(b)))
+
+# t=(1,)
+# print(t)
+# print(type(t))
+
+# class 
+
+
+
+
+# def new(func):
+#     def wrapper():
+#         text = func()
+
+#         result = ""
+#         upper = True
+
+#         for ch in text:
+#             if ch.isalpha():
+#                 if upper:
+#                     result += ch.upper()
+#                 else:
+#                     result += ch.lower()
+#                 upper = not upper
+#             else:
+#                 result += ch
+
+#         print(result)
+#     return wrapper
+
+
+# @new
+# def aa():
+#     return "goood afternoon"
+
+
+# aa()
+
+
+text = "manhar gurukkal"
+print(text.capitalize())
+# aa=text.replace("har","HAR")
+
+
+# print(aa)
