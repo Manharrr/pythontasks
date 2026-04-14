6ed509671b052ee5f5cf3903b19276923de620c8
ManharCK <manhargurukkal2@gmail.com>
2026-04-14 10:55:29 +0530

    new

--- oops.py
+++ oops.py
@@ -236,13 +236,3 @@ print(decor())
 #     def deposit(self, amount):
 #         if amount > 0:
 #             self.__balance += amount
-
-#     def get_balance(self):
-#         return self.__balance
-    
-# acc = BankAccount(1000)
-# print(acc.get_balance())
-
-# acc.deposit(500)
-# print(acc.get_balance())
-
